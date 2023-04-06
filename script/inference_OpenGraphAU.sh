8f938e6ee8e5628ff753c30db73394fed58bdbdf
khanghn <khanghn@ftech.ai>
2023-04-06 17:01:10 +0700

    add: inference with shell script

---
+++ script/inference_OpenGraphAU.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+python3 OpenGraphAU/demo.py --arc resnet50 --exp-name demo \
+        --resume checkpoints/OpenGprahAU-SwinT_first_stage.pth \
+        --input OpenGraphAU/demo_imgs/1014.jpg
