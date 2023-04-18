6eaa4751bb64b38794a41746d3d043e5c2e0cd74
khanghn <khanghn@ftech.ai>
2023-04-18 14:32:47 +0700

    fix: auto download pretrained it not exist

--- OpenGraphAU/demo.py
+++ OpenGraphAU/demo.py
@@ -28,16 +28,18 @@ def main(conf):
         if not os.path.exists(conf.resume):
             print("Downloading checkpoint: ", conf.resume)
             if "SwinT" in conf.resume:
-                gdown.download(
-                    id="1JSa-ft965qXJlVGvnoMepbkRkSm78_to", output="checkpoints/OpenGprahAU-SwinT_first_stage.pth"
-                )
+                gdown.download(id="1JSa-ft965qXJlVGvnoMepbkRkSm78_to", output="checkpoints/OpenGprahAU-SwinT_first_stage.pth")
             if "SwinS" in conf.resume:
-                gdown.download(
-                    id="1GNjFKpd00nvgYIP2q7AzRSzfzEUfAqfT", output="checkpoints/OpenGprahAU-SwinS_first_stage.pth"
-                )
+                gdown.download(id="1GNjFKpd00nvgYIP2q7AzRSzfzEUfAqfT", output="checkpoints/OpenGprahAU-SwinS_first_stage.pth")
             if "SwinB" in conf.resume:
+                gdown.download(id="1nWwowmq4pQn1ACnSOOeyBy6-n0rmqTQ9", output="checkpoints/OpenGprahAU-SwinB_first_stage.pth")
+            if "ResNet50" in conf.resume:
                 gdown.download(
-                    id="1nWwowmq4pQn1ACnSOOeyBy6-n0rmqTQ9", output="checkpoints/OpenGprahAU-SwinB_first_stage.pth"
+                    id="11xh9r2e4qCpWEtQ-ptJGWut_TQ0_AmSp", output="checkpoints/OpenGprahAU-ResNet50_first_stage.pth"
+                )
+            if "ResNet18" in conf.resume:
+                gdown.download(
+                    id="1b9yrKF663K9IwY2C2-1SD6azpAdNgBm7", output="checkpoints/OpenGprahAU-ResNet18_first_stage.pth"
                 )
 
         logging.info("Resume form | {} ]".format(conf.resume))
@@ -59,7 +61,7 @@ def main(conf):
     # log
     infostr = {"AU prediction:"}
     logging.info(infostr)
-    infostr_probs, infostr_aus = dataset_info(pred, 0.2)
+    infostr_probs, infostr_aus = dataset_info(pred, 0.5)
     logging.info(infostr_aus)
     logging.info(infostr_probs)
 
