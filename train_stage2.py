c32a68ea14478f8ae781e8a53ac7d459c34d79ed
lingjivoo <35754447+lingjivoo@users.noreply.github.com>
2023-07-03 20:27:24 +0800

    Update train_stage2.py

--- train_stage2.py
+++ train_stage2.py
@@ -12,56 +12,62 @@ from dataset import *
 from utils import *
 from conf import get_config,set_logger,set_outdir,set_env
 
-from sam import SAM
 
 def get_dataloader(conf):
     print('==> Preparing data...')
-    trainset = HybridDataset(conf.dataset_path, phase='train', transform=image_train(crop_size=conf.crop_size), stage = 1)
-    train_loader = DataLoader(trainset, batch_size=conf.batch_size, shuffle=True, num_workers=conf.num_workers)
-    valset = HybridDataset(conf.dataset_path, phase='val', transform=image_eval(crop_size=conf.crop_size), stage = 1)
-    val_loader = DataLoader(valset, batch_size=conf.batch_size, shuffle=False, num_workers=conf.num_workers)
+    if conf.dataset == 'BP4D':
+        trainset = BP4D(conf.dataset_path, train=True, fold = conf.fold, transform=image_train(crop_size=conf.crop_size), crop_size=conf.crop_size, stage = 2)
+        train_loader = DataLoader(trainset, batch_size=conf.batch_size, shuffle=True, num_workers=conf.num_workers)
+        valset = BP4D(conf.dataset_path, train=False, fold=conf.fold, transform=image_test(crop_size=conf.crop_size), stage = 2)
+        val_loader = DataLoader(valset, batch_size=conf.batch_size, shuffle=False, num_workers=conf.num_workers)
+
+    elif conf.dataset == 'DISFA':
+        trainset = DISFA(conf.dataset_path, train=True, fold = conf.fold, transform=image_train(crop_size=conf.crop_size), crop_size=conf.crop_size, stage = 2)
+        train_loader = DataLoader(trainset, batch_size=conf.batch_size, shuffle=True, num_workers=conf.num_workers)
+        valset = DISFA(conf.dataset_path, train=False, fold=conf.fold, transform=image_test(crop_size=conf.crop_size), stage = 2)
+        val_loader = DataLoader(valset, batch_size=conf.batch_size, shuffle=False, num_workers=conf.num_workers)
+
     return train_loader, val_loader, len(trainset), len(valset)
 
 
-
 # Train
-def train(conf,net,train_loader,optimizer,epoch,criterion):
+def train(conf, net, train_loader, optimizer, epoch, criterion):
     losses = AverageMeter()
+    losses1 = AverageMeter()
+    losses2 = AverageMeter()
     net.train()
     train_loader_len = len(train_loader)
-    for batch_idx, (inputs,  targets) in enumerate(tqdm(train_loader)):
+    for batch_idx, (inputs,  targets, relations) in enumerate(tqdm(train_loader)):
         adjust_learning_rate(optimizer, epoch, conf.epochs, conf.learning_rate, batch_idx, train_loader_len)
-        targets = targets.float()
+        targets, relations = targets.float(), relations.long()
         if torch.cuda.is_available():
-            inputs, targets = inputs.cuda(), targets.cuda()
-        # optimizer.zero_grad()
-        # first forward-backward step
-        enable_running_stats(net)
-        outputs = net(inputs)
-        loss = criterion(outputs, targets)
+            inputs, targets, relations = inputs.cuda(), targets.cuda(), relations.cuda()
+        optimizer.zero_grad()
+        outputs, outputs_relation = net(inputs)
+        wa_loss = criterion[0](outputs, targets)
+        edge_loss = criterion[1](outputs_relation.view(-1,4), relations.view(-1))
+        loss = wa_loss + conf.lam * edge_loss
         loss.backward()
-        optimizer.first_step(zero_grad=True)
-
-        disable_running_stats(net)
-        criterion(net(inputs), targets).backward()
-        optimizer.second_step(zero_grad=True)
-        # optimizer.step()
+        optimizer.step()
         losses.update(loss.data.item(), inputs.size(0))
-    return losses.avg
+        losses1.update(wa_loss.data.item(), inputs.size(0))
+        losses2.update(edge_loss.data.item(), inputs.size(0))
+
+    return losses.avg, losses1.avg, losses2.avg
 
 
 # Val
-def val(net,val_loader,criterion):
+def val(net, val_loader, criterion):
     losses = AverageMeter()
     net.eval()
     statistics_list = None
     for batch_idx, (inputs, targets) in enumerate(tqdm(val_loader)):
+        targets = targets.float()
         with torch.no_grad():
-            targets = targets.float()
             if torch.cuda.is_available():
                 inputs, targets = inputs.cuda(), targets.cuda()
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+            outputs, _ = net(inputs)
+            loss = criterion[0](outputs, targets)
             losses.update(loss.data.item(), inputs.size(0))
             update_list = statistics(outputs, targets.detach(), 0.5)
             statistics_list = update_statistics_list(statistics_list, update_list)
@@ -71,18 +77,18 @@ def val(net,val_loader,criterion):
 
 
 def main(conf):
-
-    dataset_info = hybrid_infolist
-
+    if conf.dataset == 'BP4D':
+        dataset_info = BP4D_infolist
+    elif conf.dataset == 'DISFA':
+        dataset_info = DISFA_infolist
 
     start_epoch = 0
-    best_val_mean_f1_score = 0
     # data
     train_loader,val_loader,train_data_num,val_data_num = get_dataloader(conf)
-    train_weight = torch.from_numpy(np.loadtxt(os.path.join(conf.dataset_path, 'list', conf.dataset+'_train_weight.txt')))
-    logging.info("[ val_data_num: {} ]".format(val_data_num))
+    train_weight = torch.from_numpy(np.loadtxt(os.path.join(conf.dataset_path, 'list', conf.dataset+'_weight_fold'+str(conf.fold)+'.txt')))
+    logging.info("Fold: [{} | {}  val_data_num: {} ]".format(conf.fold + 1, conf.N_fold, val_data_num))
+    net = MEFARG(num_classes=conf.num_classes, backbone=conf.arc)
 
-    net = MEFARG(num_main_classes=conf.num_main_classes, num_sub_classes=conf.num_sub_classes, backbone=conf.arc)
     # resume
     if conf.resume != '':
         logging.info("Resume form | {} ]".format(conf.resume))
@@ -92,24 +98,20 @@ def main(conf):
         net = nn.DataParallel(net).cuda()
         train_weight = train_weight.cuda()
 
-    criterion = WeightedAsymmetricLoss(weight=train_weight)
-
-    # optimizer = optim.AdamW(net.parameters(),  betas=(0.9, 0.999), lr=conf.learning_rate, weight_decay=conf.weight_decay)
-    optimizer = SAM(net.parameters(), optim.AdamW, rho=2.0, adaptive=True, lr=conf.learning_rate, weight_decay=conf.weight_decay,  betas=(0.9, 0.999))
-
+    criterion = [WeightedAsymmetricLoss(weight=train_weight), nn.CrossEntropyLoss()]
+    optimizer = optim.AdamW(net.parameters(),  betas=(0.9, 0.999), lr=conf.learning_rate, weight_decay=conf.weight_decay)
     print('the init learning rate is ', conf.learning_rate)
 
     #train and val
     for epoch in range(start_epoch, conf.epochs):
         lr = optimizer.param_groups[0]['lr']
         logging.info("Epoch: [{} | {} LR: {} ]".format(epoch + 1, conf.epochs, lr))
-        train_loss = train(conf, net, train_loader, optimizer, epoch, criterion)
+        train_loss, wa_loss, edge_loss = train(conf,net,train_loader,optimizer,epoch,criterion)
         val_loss, val_mean_f1_score, val_f1_score, val_mean_acc, val_acc = val(net, val_loader, criterion)
 
         # log
-        infostr = {'Epoch:  {}   train_loss: {:.5f}  val_loss: {:.5f}  val_mean_f1_score {:.2f}, val_mean_acc {:.2f}'
-                .format(epoch + 1, train_loss, val_loss, 100.* val_mean_f1_score, 100.* val_mean_acc)}
-
+        infostr = {'Epoch:  {}   train_loss: {:.5f} wa_loss: {:.5f} edge_loss: {:.5f} val_loss: {:.5f}  val_mean_f1_score {:.2f},val_mean_acc {:.2f}'
+                .format(epoch + 1, train_loss, wa_loss, edge_loss, val_loss, 100.* val_mean_f1_score, 100.* val_mean_acc)}
         logging.info(infostr)
         infostr = {'F1-score-list:'}
         logging.info(infostr)
@@ -121,21 +123,20 @@ def main(conf):
         logging.info(infostr)
 
         # save checkpoints
-        if best_val_mean_f1_score < val_mean_f1_score:
-            best_val_mean_f1_score = val_mean_f1_score
+        if (epoch+1) % 4 == 0:
             checkpoint = {
                 'epoch': epoch,
                 'state_dict': net.state_dict(),
                 'optimizer': optimizer.state_dict(),
             }
-            torch.save(checkpoint, os.path.join(conf['outdir'], 'best_model.pth'))
+            torch.save(checkpoint, os.path.join(conf['outdir'], 'epoch' + str(epoch + 1) + '_model_fold' + str(conf.fold + 1) + '.pth'))
 
         checkpoint = {
             'epoch': epoch,
             'state_dict': net.state_dict(),
             'optimizer': optimizer.state_dict(),
         }
-        torch.save(checkpoint, os.path.join(conf['outdir'], 'cur_model.pth'))
+        torch.save(checkpoint, os.path.join(conf['outdir'], 'cur_model_fold' + str(conf.fold + 1) + '.pth'))
 
 
 # ---------------------------------------------------------------------------------
@@ -149,4 +150,3 @@ if __name__=="__main__":
     # Set the logger
     set_logger(conf)
     main(conf)
-
